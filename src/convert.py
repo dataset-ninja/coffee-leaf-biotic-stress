ec3d6f6bf8d96ac47edfb34df77d2cfc6fb177fe
GoldenAnpu <seryios.sych@gmail.com>
2023-06-08 18:41:11 +0200

    add convert_and_upload_supervisely_project

--- src/convert.py
+++ src/convert.py
@@ -5,3 +5,15 @@ api = sly.Api.from_env()
 project_id = to_supervisely(api)
 
 print("Project id is", project_id)
+
+
+def convert_and_upload_supervisely_project(
+    api: sly.Api, workspace_id: int, project_name: str
+) -> sly.ProjectInfo:
+    # * dataset_path = "Path to the local dir with dataset"
+
+    # Function should read local dataset and upload it to Supervisely project, then return project info.
+
+    raise NotImplementedError("The converter should be implemented manually.")
+
+    # * return project
